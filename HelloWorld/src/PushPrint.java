be2f5774a596c5406193b58b788a9afdfabf6c58
MGT4869 <47241183+MGT4869@users.noreply.github.com>
2022-04-07 11:04:11 +0900

    remoteupdate

--- HelloWorld/src/PushPrint.java
+++ HelloWorld/src/PushPrint.java
@@ -3,5 +3,6 @@ public class PushPrint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("file add");
+		System.out.println("file add2");
 	}
 }
